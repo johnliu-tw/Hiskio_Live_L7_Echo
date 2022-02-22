12fa1e6e944efd91dc1ecba4f3bb98fa96143400
johnliu <johnliutw@hotmail.com>
2022-02-22 23:28:21 +0800

    Step 5: add user `here` and `joining` feature

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -2,14 +2,29 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Test</h1>
+<h3>Current User</h3>
+<div id="users">
+
+</div>
+<h3>Current Message</h3>
+<div id="chats">
+    
+</div>
 <input type="text" id="text">
 <button id="submit" onclick="sentMessage()">送出</button>
 <script type="application/javascript">
   window.onload = function(){
     Echo.join('chat')
-      .here((user) => {
-        console.log(user);
+      .here((users) => {
+        users.forEach(function(user){
+            createUserElement(user)
+        })
+      })
+      .joining((user) => {
+        createUserElement(user)
+      })
+      .leaving((user) => {
+          console.log(user);
       })
       .listen('SentMessage', (data) => {
           console.log(data);
@@ -18,6 +33,17 @@
           console.error(error);
       });
   }
+
+  function createUserElement(user){
+    let newElement = document.createElement('p');
+    userElement = document.getElementById('users')
+    newElement.setAttribute("id", user.email);
+    newElement.textContent = user.name
+    if(document.getElementById(user.email) === null){
+        userElement.append(newElement)
+    }
+  }
+
   function sentMessage(){
     let message = document.getElementById('text').value
     fetch('/send-message', {
