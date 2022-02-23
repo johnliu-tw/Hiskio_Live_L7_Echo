aa8c93e17af967e2b01e9a04766c6623ce7f9f60
johnliu <johnliutw@hotmail.com>
2022-02-23 21:36:34 +0800

    Step 7: add whisper function

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -3,18 +3,17 @@
 
 @section('content')
 <h3>Current User</h3>
-<div id="users">
-
-</div>
+<div id="users"></div>
 <h3>Current Message</h3>
 <div id="chats">
     @foreach ($data as $message)
         <p>{{ $message->created_at }} {{ $message->user->name }} {{ $message->message }}</p>
     @endforeach
 </div>
-<input type="text" id="text">
+<input type="text" id="text" onkeyup="sentTyping()">
 <button id="submit" onclick="sentMessage()">送出</button>
 <script type="application/javascript">
+  let userEmail = "{{ auth()->user()->email }}"
   window.onload = function(){
     Echo.join('chat')
       .here((users) => {
@@ -33,6 +32,9 @@
         chatElement = document.getElementById('chats')
         newElement.textContent = `${data.message.created_at} ${data.user.name}: ${data.message.message}`
         chatElement.append(newElement)
+      })
+      .listenForWhisper('typing', (e) => {
+        console.log(e.email);
       })
       .error((error) => {
           console.error(error);
@@ -58,6 +60,13 @@
             'content-type': 'application/json'
         },
     })
+  }
+
+  function sentTyping(){
+    Echo.join('chat')
+        .whisper('typing', {
+            email: userEmail
+        });
   }
 
 </script>
